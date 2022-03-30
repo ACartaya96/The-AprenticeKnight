eb13ed88504d2442225e1c2682578e9d01a8b499
ACartaya96 <87090729+ACartaya96@users.noreply.github.com>
2022-03-30 16:29:51 -0400

    Set Up some of the logic scripts

--- Assets/Scripts/Enemy Scripts/CombatStanceState.cs
+++ Assets/Scripts/Enemy Scripts/CombatStanceState.cs
@@ -7,15 +7,35 @@ namespace TAK
     [CreateAssetMenu(menuName = "A.I./Enemy Actions/ States/ Combat Stance")]
     public class CombatStanceState : EnemyBaseState
     {
+        public AttackState attackState;
+        public PursueTargetState pursueTarget;
         public override EnemyBaseState Tick(EnemyManager enemyManager, EnemyStats enemyStats, EnemyAnimationHandler enemyAnimationHandler, FieldofView fov)
         {
+            enemyManager.distanceFromTarget = Vector3.Distance(enemyManager.currentTarget.transform.position, enemyManager.transform.position);
             //Check for attack range
-            //circle player or walk around them until ready to attack player
             //if in attack range switch to attack State
-            //if weare in a cooldown after attacking, return this state so that we continue to circle the player
+            if (enemyManager.currentRecoveryTime <= 0 && enemyManager.distanceFromTarget <= enemyManager.maximumAttackRange)
+            {
+                return attackState;
+            }
             //is the player runs out of ranger switch to the Pursue Target State
+            else if (enemyManager.distanceFromTarget > enemyManager.maximumAttackRange)
+            {
+                return pursueTarget;
+            }
 
-            return this;
+            //if weare in a cooldown after attacking, return this state so that we continue to circle the player
+            else
+            {
+                return this;
+            }
+
+            //circle player or walk around them until ready to attack player(WIP)
+
+           
+            
+
+           
         }
     }
 }
