1d42c2f9ecc1446aadabebaabd7c8c175b190165
ACartaya96 <87090729+ACartaya96@users.noreply.github.com>
2022-03-31 15:57:26 -0400

    AI Controller Basic Complete

--- Assets/Scripts/Enemy Scripts/EnemyIdleState.cs
+++ Assets/Scripts/Enemy Scripts/EnemyIdleState.cs
@@ -21,18 +21,89 @@ namespace TAK
 
             
             //Look for a potential target
+           
+            if (enemyManager.isPerformingAction)
+                return this;
+
             fov.FieldOFViewCheck();
+
+            Vector3 targetDirection = wayPoints[enemyManager.WayPointIndex].position - enemyManager.transform.position;
+            float distanceFromTarget = Vector3.Distance(wayPoints[enemyManager.WayPointIndex].position, enemyManager.transform.position);
+           float viewAngle = Vector3.Angle(targetDirection, enemyManager.transform.forward);
+
+            //If we are performing action(ex. Attack,Dodge,Spell,etc.) we turn of navmesh so that
+            //the enemy can rotate towards its target with out worry of pathfinding.
+            if (enemyManager.isPerformingAction)
+            {
+                enemyAnimationHandler.anim.SetFloat("Vertical", 0, 0.1f, Time.deltaTime);
+                enemyManager.navMeshAgent.enabled = false;
+
+            }
+            else
+            {
+                if (distanceFromTarget > enemyManager.maximumAttackRange)
+                {
+                    enemyAnimationHandler.anim.SetFloat("Vertical", 0.5f, 0.1f, Time.deltaTime);
+
+                }
+                else if (distanceFromTarget <= enemyManager.maximumAttackRange)
+                {
+                    enemyAnimationHandler.anim.SetFloat("Vertical", 0, 0.1f, Time.deltaTime);
+
+                }
+
+            }
+            HandleRotateTowardsTarget(enemyManager);
+            enemyManager.navMeshAgent.nextPosition = enemyManager.transform.position;
+            enemyManager.navMeshAgent.transform.localRotation = Quaternion.identity;
+
+            if(distanceFromTarget <= enemyManager.maximumAttackRange)
+            {
+                if(enemyManager.WaitTime <= 0)
+                {
+                    enemyManager.WayPointIndex = (enemyManager.WayPointIndex + 1) % wayPoints.Length;
+                    currentWayPoint = wayPoints[enemyManager.WayPointIndex];
+                    enemyManager.navMeshAgent.speed = speedWalk;
+                }
+                else
+                {
+                   
+                    enemyManager.navMeshAgent.speed = 0;
+                    enemyManager.WaitTime -= Time.deltaTime;
+                }
+            }
+            //If we are in attack range (must be created), switch to Combat State
             if (enemyManager.currentTarget != null)
             {
-                //Switch to a pursue target state if it finds a target
-                enemyManager.navMeshAgent.enabled = false;
+                enemyManager.navMeshAgent.ResetPath();
                 return pursueTargetState;
             }
             else
             {
-                enemyManager.navMeshAgent.enabled = true;
-                currentWayPoint = wayPoints[enemyManager.WayPointIndex];
-                enemyManager.distanceFromTarget = Vector3.Distance(enemyManager.transform.position , wayPoints[enemyManager.WayPointIndex].position);
+                return this;
+            }
+
+        }
+
+        private void HandleRotateTowardsTarget(EnemyManager enemyManager)
+        {
+            //Rotate Manually
+            if (enemyManager.isPerformingAction)
+            {
+                Vector3 direction = wayPoints[enemyManager.WayPointIndex].position - enemyManager.transform.position;
+                direction.y = 0;
+                direction.Normalize();
+
+                if (direction == Vector3.zero)
+                {
+                    direction = enemyManager.transform.forward;
+                }
+
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                enemyManager.transform.rotation = Quaternion.Slerp(enemyManager.transform.rotation, targetRotation, enemyManager.rotationSpeed / Time.deltaTime);
+            }
+            else
+            {
                 Vector3 relativeDirection = enemyManager.transform.InverseTransformDirection(enemyManager.navMeshAgent.desiredVelocity);
                 Vector3 targetVelocity = enemyManager.rb.velocity;
 
@@ -40,48 +111,17 @@ namespace TAK
                 enemyManager.navMeshAgent.SetDestination(currentWayPoint.position);
                 enemyManager.rb.velocity = targetVelocity;
                 enemyManager.transform.rotation = Quaternion.Slerp(enemyManager.transform.rotation, enemyManager.navMeshAgent.transform.rotation, enemyManager.rotationSpeed / Time.deltaTime);
-
-                enemyAnimationHandler.anim.SetFloat("Vertical", 0.5f, 0.1f, Time.deltaTime);
-                if(  enemyManager.distanceFromTarget <=   enemyManager.stoppingDistance)
-                {
-                    if(enemyManager.WaitTime <= 0)
-                    {
-                        NextPoint(enemyManager);
-                        Move(enemyManager,speedWalk);
-                        enemyManager.WaitTime = enemyManager.startWaitTime;
-                    }
-                    else
-                    {
-                       
-                        Stop(enemyManager);
-                        enemyAnimationHandler.anim.SetFloat("Vertical", 0f, 0.01f, Time.deltaTime);
-                        
-                        enemyManager.WaitTime -= Time.deltaTime;
-                    }
-                    
-                }
-                return this;
             }
+
+
+
+         
+        }
             
             
-        }
-        void Move(EnemyManager enemyManager,float speed)
-        {
-            enemyManager.navMeshAgent.isStopped = false;
-            enemyManager.navMeshAgent.speed = speed;
-        }
-
-        void Stop(EnemyManager enemyManager)
-        {
-
-            enemyManager.navMeshAgent.isStopped = true;
-            enemyManager.navMeshAgent.speed = 0;
-        }
-
-        void NextPoint(EnemyManager enemyManager)
-        {
-            enemyManager.WayPointIndex = (enemyManager.WayPointIndex + 1) % wayPoints.Length;
-            enemyManager.navMeshAgent.SetDestination(wayPoints[enemyManager.WayPointIndex].position);
-        }
     }
+       
+
+     
 }
+
