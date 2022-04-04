37a302cd20fe541a1348ba852fcb0716b5e43416
k-horan <kevinhoran@knights.ucf.edu>
2022-04-03 22:33:55 -0400

    Hag Audio Changes
    Added Sounds for the attacks and injured animations

--- Assets/Scripts/Managers/EnemyAudioManager.cs
+++ Assets/Scripts/Managers/EnemyAudioManager.cs
@@ -9,16 +9,34 @@ namespace TAK
     {
         public SFXEvent footStepSFX;
         public SFXEvent weaponSwingSFX;
+        public SFXEvent injuredScreamSFX;
+        public SFXEvent swipeAttackSFX;
+        public SFXEvent bigSlamAttackSFX;
+        public SFXEvent spinAttackSFX;
 
         public void PlayFootStepAudio()
         {
-
             footStepSFX?.Play();
         }
 
-        public void PlaySwingAudio()
+        public void PlaySwipeAttackAudio()
         {
+            swipeAttackSFX.Play();
+        }
 
+        public void PlayInjuredAudio()
+        {
+            injuredScreamSFX.Play();
+        }
+
+        public void PlayBigSlamAudio()
+        {
+            bigSlamAttackSFX.Play();
+        }
+
+        public void PlaySpinAttackAudio()
+        {
+            spinAttackSFX.Play();
         }
     }
 }
