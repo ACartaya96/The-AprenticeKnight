6dfd62b43631b91da23a14aea0d0da8052ee7fe2
RochbergB <benrochberg@gmail.com>
2022-04-17 21:08:57 -0400

    Audio Slider saves changes
    All audio sliders have a start value for the first time the game is opened, all changes are saved. Audio isn't linked yet

--- Assets/Scripts/UI/AudioManager.cs
+++ Assets/Scripts/UI/AudioManager.cs
@@ -2,11 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string MasterPref = "MasterPref";
+    private static readonly string PlayEffectsPref = "PlayEffectsPref";
+    private static readonly string GameEffectsPref = "GameEffectsPref";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private int firstPlayInt;
+    public Slider MasterSlider, PlayEffectsSlider, GameEffectsSlider, BackgroundSlider;
+    private float MasterFloat, PlayEffectsFloat, GameEffectsFloat, BackgroundFloat;
     public AudioMixer audioMixer;
 
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+        if(firstPlayInt == 0)
+        {
+            MasterFloat = 0.125f;
+            PlayEffectsFloat = 0.50f;
+            GameEffectsFloat = 0.50f;
+            BackgroundFloat = 0.25f;
+            MasterSlider.value = MasterFloat;
+            PlayEffectsSlider.value = PlayEffectsFloat;
+            GameEffectsSlider.value = GameEffectsFloat;
+            BackgroundSlider.value = BackgroundFloat;
+            PlayerPrefs.SetFloat(MasterPref, MasterFloat);
+            PlayerPrefs.SetFloat(PlayEffectsPref, PlayEffectsFloat);
+            PlayerPrefs.SetFloat(GameEffectsPref, GameEffectsFloat);
+            PlayerPrefs.SetFloat(BackgroundPref, BackgroundFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+            MasterFloat = PlayerPrefs.GetFloat(MasterPref);
+            MasterSlider.value = MasterFloat;
+            PlayEffectsFloat = PlayerPrefs.GetFloat(PlayEffectsPref);
+            PlayEffectsSlider.value = PlayEffectsFloat;
+            GameEffectsFloat = PlayerPrefs.GetFloat(GameEffectsPref);
+            GameEffectsSlider.value = GameEffectsFloat;
+            BackgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            BackgroundSlider.value = BackgroundFloat;
+        }
+    }
 
     public void SetMasterAudio (float volume)
     {
@@ -28,6 +68,26 @@ public class AudioManager : MonoBehaviour
         audioMixer.SetFloat("BackgroundVolume", volume);
     }
 
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(MasterPref, MasterSlider.value);
+        PlayerPrefs.SetFloat(PlayEffectsPref, PlayEffectsSlider.value);
+        PlayerPrefs.SetFloat(GameEffectsPref, GameEffectsSlider.value);
+        PlayerPrefs.SetFloat(BackgroundPref, BackgroundSlider.value);
+    }
+
+    void OnApplicationFocus(bool inFocus)
+    {
+        if(!inFocus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+    public void UpdateSound()
+    {
+    
+    }
 
 
 
