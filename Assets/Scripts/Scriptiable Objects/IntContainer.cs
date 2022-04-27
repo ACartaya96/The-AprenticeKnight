c8337706bc74e2c1c246fa62f974468629570e9c
RochbergB <benrochberg@gmail.com>
2022-04-27 15:59:22 -0400

    Audio Slider Push

---
+++ Assets/Scripts/Scriptiable Objects/IntContainer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TAK
+{
+
+    [CreateAssetMenu(menuName = "data/int")]
+public class IntContainer : ScriptableObject
+{
+    public int MaxInt, currentInt;
+
+
+
+}
+}
--- Assets/Scripts/UI/AudioMixerMaster.cs
+++ Assets/Scripts/UI/AudioMixerMaster.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
+namespace TAK
+{
 public class AudioMixerMaster : MonoBehaviour
 {
     private static readonly string FirstPlay = "FirstPlay";
@@ -11,40 +13,54 @@ public class AudioMixerMaster : MonoBehaviour
     private static readonly string PlayEffectsPref = "PlayEffectsPref";
     private static readonly string GameEffectsPref = "GameEffectsPref";
     private static readonly string BackgroundPref = "BackgroundPref";
-    private int firstPlayInt;
+    //private int firstPlayInt;
     public Slider MasterSlider, PlayEffectsSlider, GameEffectsSlider, BackgroundSlider;
-    private float MasterFloat, PlayEffectsFloat, GameEffectsFloat, BackgroundFloat;
+    //public float MasterFloat, PlayEffectsFloat, GameEffectsFloat, BackgroundFloat;
+
+    public FloatContainer MasterFloat, PlayEffectsFloat, GameEffectsFloat, BackgroundFloat;
+
+    public IntContainer firstPlayInt;
     public AudioMixer audioMixer;
 
     void Start()
     {
-        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
-        if(firstPlayInt == 0)
+         //Debug.Log(firstPlayInt.currentInt);
+         firstPlayInt.currentInt = PlayerPrefs.GetInt(FirstPlay);
+         if(firstPlayInt.currentInt == 0)
+         {
+            MasterFloat.currentFloat = MasterFloat.MaxFloat;
+            PlayEffectsFloat.currentFloat = PlayEffectsFloat.MaxFloat;
+            GameEffectsFloat.currentFloat = GameEffectsFloat.MaxFloat;
+            BackgroundFloat.currentFloat = BackgroundFloat.MaxFloat;
+            MasterSlider.value = MasterFloat.currentFloat;
+            PlayEffectsSlider.value = PlayEffectsFloat.currentFloat;
+            GameEffectsSlider.value = GameEffectsFloat.currentFloat;
+            BackgroundSlider.value = BackgroundFloat.currentFloat;
+            PlayerPrefs.SetFloat(MasterPref, MasterFloat.currentFloat);
+            PlayerPrefs.SetFloat(PlayEffectsPref, PlayEffectsFloat.currentFloat);
+            PlayerPrefs.SetFloat(GameEffectsPref, GameEffectsFloat.currentFloat);
+            PlayerPrefs.SetFloat(BackgroundPref, BackgroundFloat.currentFloat);
+            PlayerPrefs.SetInt(FirstPlay, (firstPlayInt.currentInt -1));
+         }
+         else
         {
-            MasterFloat = 0.125f;
-            PlayEffectsFloat = 0.50f;
-            GameEffectsFloat = 0.50f;
-            BackgroundFloat = 0.25f;
-            MasterSlider.value = MasterFloat;
-            PlayEffectsSlider.value = PlayEffectsFloat;
-            GameEffectsSlider.value = GameEffectsFloat;
-            BackgroundSlider.value = BackgroundFloat;
-            PlayerPrefs.SetFloat(MasterPref, MasterFloat);
-            PlayerPrefs.SetFloat(PlayEffectsPref, PlayEffectsFloat);
-            PlayerPrefs.SetFloat(GameEffectsPref, GameEffectsFloat);
-            PlayerPrefs.SetFloat(BackgroundPref, BackgroundFloat);
-            PlayerPrefs.SetInt(FirstPlay, -1);
-        }
-        else
-        {
-            MasterFloat = PlayerPrefs.GetFloat(MasterPref);
-            MasterSlider.value = MasterFloat;
-            PlayEffectsFloat = PlayerPrefs.GetFloat(PlayEffectsPref);
-            PlayEffectsSlider.value = PlayEffectsFloat;
-            GameEffectsFloat = PlayerPrefs.GetFloat(GameEffectsPref);
-            GameEffectsSlider.value = GameEffectsFloat;
-            BackgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
-            BackgroundSlider.value = BackgroundFloat;
+            // PlayerPrefs.SetFloat(MasterPref, MasterFloat);
+            // PlayerPrefs.SetFloat(PlayEffectsPref, PlayEffectsFloat);
+            // PlayerPrefs.SetFloat(GameEffectsPref, GameEffectsFloat);
+            // PlayerPrefs.SetFloat(BackgroundPref, BackgroundFloat);
+            MasterFloat.currentFloat = PlayerPrefs.GetFloat(MasterPref);
+            MasterSlider.value = MasterFloat.currentFloat;
+            PlayEffectsFloat.currentFloat = PlayerPrefs.GetFloat(PlayEffectsPref);
+            PlayEffectsSlider.value = PlayEffectsFloat.currentFloat;
+            GameEffectsFloat.currentFloat = PlayerPrefs.GetFloat(GameEffectsPref);
+            GameEffectsSlider.value = GameEffectsFloat.currentFloat;
+            BackgroundFloat.currentFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            BackgroundSlider.value = BackgroundFloat.currentFloat;
+            // MasterSlider.value = MasterFloat;
+            // PlayEffectsSlider.value = PlayEffectsFloat;
+            // GameEffectsSlider.value = GameEffectsFloat;
+            // BackgroundSlider.value = BackgroundFloat;
+            
         }
     }
 
@@ -70,22 +86,24 @@ public class AudioMixerMaster : MonoBehaviour
 
     public void SaveSoundSettings()
     {
+        Debug.Log("SettingSaved");
         PlayerPrefs.SetFloat(MasterPref, MasterSlider.value);
         PlayerPrefs.SetFloat(PlayEffectsPref, PlayEffectsSlider.value);
         PlayerPrefs.SetFloat(GameEffectsPref, GameEffectsSlider.value);
         PlayerPrefs.SetFloat(BackgroundPref, BackgroundSlider.value);
     }
 
-    void OnApplicationFocus(bool inFocus)
-    {
-        if(!inFocus)
-        {
-            SaveSoundSettings();
-        }
-    }
+    // void OnApplicationFocus(bool inFocus)
+    // {
+    //     if(!inFocus)
+    //     {
+    //         SaveSoundSettings();
+    //     }
+    // }
 
 
 
 
 
+}
 }
